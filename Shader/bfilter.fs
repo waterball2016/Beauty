0147ea5ba4cf702cb8361e07c48f4bdf31b72b59
Chen Xi <chenxi@cdv.com>
2017-02-15 14:37:19 +0800

    Add Muliplier to Bilateral filter

--- Shader/bfilter.fs
+++ Shader/bfilter.fs
@@ -7,7 +7,8 @@ precision mediump int;
 
 #define SIGMA 10.0
 #define BSIGMA 0.1
-#define MSIZE 13
+#define MSIZE 9
+#define MULTIPLIER 2.0
 
 in vec2 TexCoord0;
 uniform sampler2D bfSampler0;
@@ -32,7 +33,7 @@ void main(void)
 	float grad = texture2D( bfSampler1, vec2(0.0, 0.0) + TexCoord0).r;
 	float smoother = 1;
 
-	if (grad < 0.2 && c.r > 0.3725 && c.g > 0.1568 && c.b > 0.0784 && c.r > c.b && max(max(c.r, c.g), c.b) - min(min(c.r, c.g), c.b) > 0.0588 && abs(c.r - c.g) > 0.0588)
+	if (grad < 0.4 && c.r > 0.3725 && c.g > 0.1568 && c.b > 0.0784 && c.r > c.b && max(max(c.r, c.g), c.b) - min(min(c.r, c.g), c.b) > 0.0588 && abs(c.r - c.g) > 0.0588)
 	{
 		//declare stuff
 		const int kSize = (MSIZE-1)/2;
@@ -43,7 +44,7 @@ void main(void)
 		float Z = 0.0;
 		for (int j = 0; j <= kSize; ++j)
 		{
-			kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j), SIGMA);
+			kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j*MULTIPLIER), SIGMA);
 		}
 
 
@@ -53,9 +54,8 @@ void main(void)
 		{
 			for (int j=-kSize; j <= kSize; ++j)
 			{
-				cc = texture2D(bfSampler0, vec2(0.0, 0.0) + TexCoord0 + vec2(float(i),float(j)) / sketchSize.xy ).rgb;
-				factor = normpdf3(cc-c, BSIGMA) * kernel[kSize+i] * kernel[kSize+j];;	//Nicer one
-				//factor = (0.18 + i*0.03) * (0.18 + i*0.03) * (1.0 - min(distance(cc, c), 1.0));		//Efficient one
+				cc = texture2D(bfSampler0, TexCoord0 + vec2(float(i),float(j)) * MULTIPLIER / sketchSize.xy ).rgb;
+				factor = normpdf3(cc-c, BSIGMA) * kernel[kSize+i] * kernel[kSize+j];
 				Z += factor;
 				final_colour += factor*cc;
 			}
